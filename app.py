5d698bd48d30b835e186528816d35a606ed044fa
Dylan Adams <dadams10642@outlook.com>
2024-07-07 09:02:00 -0500

    revert to prod deploy config

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, Response
 import sqlite3
 from openai import OpenAI
 import random
