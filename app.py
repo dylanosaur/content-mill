391e890a64ef07ce8e41eba2b3b705f0872b5adf
Dylan Adams <dadams10642@outlook.com>
2024-07-07 08:45:42 -0500

    revert to prod deploy config

--- app.py
+++ app.py
@@ -137,5 +137,5 @@ def page(page_id):
     return render_template('page.html', page=page)
 
 if __name__ == '__main__':
-    # app.run(debug=True, host='0.0.0.0', port=6000)
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=6000)
+    # app.run()
